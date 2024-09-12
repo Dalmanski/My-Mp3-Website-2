4d7b5333e8267ebacdafd75e57511eb3a0d05e2b
Dalmanski <jayraldjohndalman@gmail.com>
2024-09-12 20:27:03 +0800

    modified:   .vscode/settings.json 	new file:   My Music/hi 	deleted:    Preview.png 	modified:   README.md 	modified:   main.html 	modified:   script.js 	modified:   styles.css

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 demo [here](https://raw.githack.com/Dalmanski/My-Mp3-Website-2/main/main.html)
+
--- script.js
+++ script.js
@@ -95,3 +95,4 @@ document.addEventListener('DOMContentLoaded', () => {
     loadTrack(currentTrackIndex);
 });
 
+
